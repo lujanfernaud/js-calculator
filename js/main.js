5615aa9b61a8212e257ffecc652d56a8367a8854
lujanfernaud <lujanfernaud@gmail.com>
2018-08-19 13:50:06 +0100

    Initial implementation of main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,42 @@
 class Calculator {
-  run() {
-    //
+  constructor() {
+    this.display = 0
+  }
+
+  operate(operator, number1, number2) {
+    switch (operator) {
+      case '+':
+        return this.add(number1, number2)
+      case '-':
+        return this.subtract(number1, number2)
+      case '*':
+        return this.multiply(number1, number2)
+      case '/':
+        return this.divide(number1, number2)
+      default:
+        return this.notValid()
+    }
+  }
+
+  add(number1, number2) {
+    return number1 + number2
+  }
+
+  subtract(number1, number2) {
+    return number1 - number2
+  }
+
+  multiply(number1, number2) {
+    return number1 * number2
+  }
+
+  divide(number1, number2) {
+    return number1 / number2
+  }
+
+  notValid() {
+    return 'Not valid.'
   }
 }
 
-new Calculator().run()
+const calculator = new Calculator()
