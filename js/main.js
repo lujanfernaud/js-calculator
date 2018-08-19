4ccae8d0d545da3553779c95bd34e72921b2bde5
lujanfernaud <lujanfernaud@gmail.com>
2018-08-19 14:40:23 +0100

    Initial implementation of buttons functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 class Calculator {
   constructor() {
-    this.display = 0
+    this.toDisplay = []
+    this.screen = document.getElementById('screen')
+    this.buttons = document.querySelectorAll('#button')
+    this.buttonExecute = document.getElementById('button-execute')
+    this.buttonClear = document.getElementById('button-clear')
+  }
+
+  start() {
+    this.watch()
+  }
+
+  watch() {
+    this.watchNormalButtons()
+    this.watchExecuteButton()
+    this.watchClearButton()
+  }
+
+  watchNormalButtons() {
+    this.buttons.forEach(button => {
+      let buttonValue = button.dataset.value
+
+      button.addEventListener('click', () => {
+        this.toDisplay.push(buttonValue)
+        this.displayValues()
+      })
+    })
+  }
+
+  displayValues() {
+    this.screen.innerHTML = this.toDisplay.join('')
+  }
+
+  watchExecuteButton() {
+    this.buttonExecute.addEventListener('click', () => {
+      console.log('Execute')
+    })
+  }
+
+  watchClearButton() {
+    this.buttonClear.addEventListener('click', () => {
+      this.toDisplay = []
+      this.screen.innerHTML = [0]
+    })
   }
 
   operate(operator, number1, number2) {
@@ -39,4 +81,4 @@ class Calculator {
   }
 }
 
-const calculator = new Calculator()
+const calculator = new Calculator().start()
