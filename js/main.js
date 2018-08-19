4f6ad121866006e0bcdca8b6c807742c29f78d90
lujanfernaud <lujanfernaud@gmail.com>
2018-08-19 17:58:51 +0100

    Implement calculator

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+const OPERATIONS = /(\d+\D\d+)(\D\d+)?(\D\d+)?(\D\d+)?(\D\d+)?/
+
 class Calculator {
   constructor() {
     this.toDisplay = []
+    this.toExecute = []
+    this.result = 0
+
     this.screen = document.getElementById('screen')
     this.buttons = document.querySelectorAll('#button')
     this.buttonExecute = document.getElementById('button-execute')
@@ -8,13 +13,9 @@ class Calculator {
   }
 
   start() {
-    this.watch()
-  }
-
-  watch() {
     this.watchNormalButtons()
-    this.watchExecuteButton()
     this.watchClearButton()
+    this.watchExecuteButton()
   }
 
   watchNormalButtons() {
@@ -32,53 +33,108 @@ class Calculator {
     this.screen.innerHTML = this.toDisplay.join('')
   }
 
-  watchExecuteButton() {
-    this.buttonExecute.addEventListener('click', () => {
-      console.log('Execute')
-    })
-  }
-
   watchClearButton() {
     this.buttonClear.addEventListener('click', () => {
-      this.toDisplay = []
-      this.screen.innerHTML = [0]
+      this.clearState()
     })
   }
 
-  operate(operator, number1, number2) {
+  clearState() {
+    this.toDisplay = []
+    this.screen.innerHTML = [0]
+  }
+
+  watchExecuteButton() {
+    this.buttonExecute.addEventListener('click', () => {
+      this.prepareOperationsToExecute()
+      this.executeOperations()
+      this.displayResult()
+    })
+  }
+
+  prepareOperationsToExecute() {
+    const operations = this.toDisplay.join('')
+
+    this.toExecute = operations
+      .match(OPERATIONS)
+      .slice(1)
+      .filter(item => item !== undefined)
+  }
+
+  executeOperations() {
+    while (this.toExecute.length > 0) {
+      let operation = this.toExecute.shift()
+      let operator = operation.match(/\D/)[0]
+      let values = operation.match(/\d+/g)
+
+      if (values.length > 1) {
+        this.operate(operator, values[0], values[1])
+      } else {
+        this.operate(operator, this.result, values[0])
+      }
+    }
+  }
+
+  displayResult() {
+    if (this.result === 0) { this.clearState() }
+
+    if (isInteger(this.result)) {
+      this.screen.innerHTML = this.result
+    } else {
+      this.screen.innerHTML = this.result.toFixed(2)
+    }
+  }
+
+  operate(operator, value1, value2) {
     switch (operator) {
+      case '%':
+        return this.modulo(value1, value2)
       case '+':
-        return this.add(number1, number2)
+        return this.add(value1, value2)
       case '-':
-        return this.subtract(number1, number2)
+        return this.subtract(value1, value2)
       case '*':
-        return this.multiply(number1, number2)
+        return this.multiply(value1, value2)
       case '/':
-        return this.divide(number1, number2)
+        return this.divide(value1, value2)
       default:
         return this.notValid()
     }
   }
 
-  add(number1, number2) {
-    return number1 + number2
+  modulo(value1, value2) {
+    this.result = Number(value1) % Number(value2)
+    this.toDisplay = [this.result]
   }
 
-  subtract(number1, number2) {
-    return number1 - number2
+  add(value1, value2) {
+    this.result = Number(value1) + Number(value2)
+    this.toDisplay = [this.result]
   }
 
-  multiply(number1, number2) {
-    return number1 * number2
+  subtract(value1, value2) {
+    this.result = Number(value1) - Number(value2)
+    this.toDisplay = [this.result]
   }
 
-  divide(number1, number2) {
-    return number1 / number2
+  multiply(value1, value2) {
+    this.result = Number(value1) * Number(value2)
+    this.toDisplay = [this.result]
+  }
+
+  divide(value1, value2) {
+    this.result = Number(value1) / Number(value2)
+    this.toDisplay = [this.result]
   }
 
   notValid() {
-    return 'Not valid.'
+    this.screen.innerHTML = 'Not valid.'
+    this.toDisplay = 0
   }
+}
+
+function isInteger(value) {
+  return Math.round(value) === value
 }
 
 const calculator = new Calculator().start()
